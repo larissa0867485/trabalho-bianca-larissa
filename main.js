6d16ff2f039bf12bcd6ceb4aa9f012c2a2b5d37b
Kauanne <161839546+larissa0867485@users.noreply.github.com>
2024-11-22 09:01:56 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -16,11 +16,10 @@ const perguntas = [
             texto: "Isso e maravilhoso!",
             afirmacao: "afirmacao"
           }
-
         ]
     },
     {
-        enunciado: "com a descoberta esta tecnologia, chamada Inteligencia Artificial (IA), uma professora de texcnologia da escola decidiu fazer uma sequencia de aulas sobre esta tecnologia. No fim de uma aula ela pede que voce escreva um trabalho sobre o uso de IA em sala de aula. Qual atitude voce toma?",
+        enunciado: "com a descoberta esta tecnologia, chamada Inteligencia Artificial, uma professora de texcnologia da escola decidiu fazer uma sequencia de aulas sobre esta tecnologia. No fim de uma aula ela pede que voce escreva um trabalho sobre o uso de IA em sala de aula. Qual atitude voce toma?",
         alternativa: [
            {
            texto: "Utiliza uma ferramenta de busca na internet que utilizava IA para que ela ajude a encontrar informaçoes relevante para o trabalho e explique numa linguagem que facilite o entendimento.",
@@ -33,34 +32,33 @@ const perguntas = [
         ]
     },
     {
-         enuncido: "apos a elaboracao do trabalho, a professora ralizou umn debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa tambem foi levantando um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como voce se posiciona?",
+         enuncido: "apos a elaboracao do trabalho escrito, a professora ralizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa tambem foi levantando um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como voce se posiciona?",
         alternativas: [
            {
-            texto: "defende a ideia de que a IA pode criar nova oportunidades de emprego e melhora habilidades humanas.",
+            texto: "defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
             afirmacao: "afirmacao"
            },
            {
-            texto: "me preoculpo com as pessoas que perderao eu empregos para maquinas e defendam a importancia de proteger os trabalhadores.",
+            texto: "me preocupo com as pessoas que perderao seus empregos para maquinas e defendem a importancia de proteger os trabalhadores.",
             afirmacao: "afirmacao"
            }
         ]        
     },
     {
-
-        enunciado:"ao final da discusao,voce precisou criar uma imagem n computador que representasse o que pensa obra AI.e agora?",
+        enunciado:"ao final da discussao,voce precisou criar uma imagem no computador que representasse o que pensa obra AI.e agora?",
         alternativas: [
             {
             texto: "criar uma imagem utilizando uma plataforma de desing como o paint.",
             afirmacao:"afirmacao"
             },
             {
-            texto: "criar uma imagem utilizando gerador de imagem IA.",
+            texto: "criar uma imagem utilizando um gerador de imagem IA.",
             afirmacao: "afirmacao"
             }
         ]   
     },
     {
-        enunciado: "voce tem um trabalho em grupo de biologia pra entregar na emana eguinte, o andamento do trabalho esta um pouco atrasado e uma pessoa do seu grupo dicidiu fazer com ajuda do IA. O problema é que o trabalho esta totalmente igual o chat, O que voce faz?",
+        enunciado: "voce tem um trabalho em grupo de biologia pra entregar na semana seguinte, o andamento do trabalho esta um pouco atrasado e uma pessoa do seu grupo dicidiu fazer com ajuda do IA. O problema é que o trabalho esta totalmente igual ao do chat, O que voce faz?",
         alternativas: [
         {
             texto: "escrever comandos para o chat e uma forma de contribuir com o trabalho, por isso nao e um problema utilizar o texto inteiro.",
@@ -68,7 +66,7 @@ const perguntas = [
         },
         {
             texto:"o chat pode ser uma tecnologia muito avancada, mas e preciso manter a atencao pois toda maquina erra, por iso revisar o trabalho e contribuir com as perspectivas pessoais é essencial",
-            afirmacao: "afirmcao"
+            afirmacao: "afirmacao"
         }
     ]
   }
@@ -76,16 +74,10 @@ const perguntas = [
 
 let atual = 0;
 let perguntaatual;
-let historiaFinal = "";
 
-function mostraPergunta(){
-  if (atual>= perguntas.length) {
-    mostraResultado();
-    return;
-  }
+function mostraPergunta(){  
     perguntaAtual = perguntas[atual];
-    caixaPerguntas.textContent = perguntaAtual.enunciado;
-    caixaAlternativas.textContent = "";
+    caixaPerguntas.textContent = perguntaAtual.enunciado;  
     mostrarAlternativas();
   }
 
@@ -93,21 +85,11 @@ function mostraAltrnativas() {
     for (const alternativa of perguntaAtual.alternativas) {
        const botaoAlternativas = document.createElement("button");
        botaoAlternativas.textContent = alternativa.texto;
-       botaoAlternativas.addEventListener("click", () => repostaSelecionada(alternativa));
-       caixaAlternativas.appendChild(botaoAltrnativas);
+       botaoAlternativas.addEventListener("click", function(){
+        atual++;
+        mostrapergunta();
+       })
+       caixaAlternativas.appendChild(botaoAlternativas);
     }  
  }
- function respostaSelecionada(opcaoSelecionada) {
-    const afirmacoes = opcaoSelecionada.afirmacoes;
-    histoiaFinal += afirmacoes + " ";
-    atual++;
-    mostrapergunta();
- }
-
- function mostraResultado() { 
-  caixaPerguntas.textContent = "em2049...";
-  textoresultado.textContent = historiaFinal;
-  caixaAlternativas.textcontent = "";
- }
-
- mostrapergunta();
+ mostraPergunta
